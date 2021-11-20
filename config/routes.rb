6ab996ec0ea728c4854d1b23af30c0950fd72b49
Alejandro Guevara <alejandro.guevara.esc@gmail.com>
2021-11-21 00:31:12 +0100

    fix: allow cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :people, only: [:index, :create] do
-    get :family
+  defaults format: :json do
+    resources :people, only: [:index, :create] do
+      get :family
+    end
   end
 end
